594e21aba1e9ba8f6f6e88fd28a8b0bc06e918d4
Stefan Wick <wick.stefan@outlook.com>
2018-03-27 22:36:13 -0700

    Adding sample 1

---
+++ UWP_FullTrust_1/FullTrust/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullTrust
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Hello World";
+            Console.WriteLine("This process runs at the full privileges of the user and has access to the entire public desktop API surface");
+            Console.WriteLine("\r\nPress any key to exit ...");
+            Console.ReadLine();
+        }
+    }
+}
